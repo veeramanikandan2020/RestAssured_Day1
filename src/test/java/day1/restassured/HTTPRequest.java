407b16626c7ff5e837b7159bb3d1bf2ca0d8bf2f
Veeramanikandan M <veeramanikandan2020@gmail.com>
2024-02-09 23:55:12 +0530

    Map Values printed using Sysout

--- src/test/java/day1/restassured/HTTPRequest.java
+++ src/test/java/day1/restassured/HTTPRequest.java
@@ -1,33 +1,35 @@
 package day1.restassured;
-	
-	import org.testng.annotations.Test;
 
-	import static io.restassured.RestAssured.*;
-	import static io.restassured.matcher.RestAssuredMatchers.*;
-	import static org.hamcrest.Matchers.*;
+import groovy.util.MapEntry;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 import java.util.HashMap;
+import java.util.Map;
 
-	public class HTTPRequest{
-		
-		int id ;
+public class HTTPRequest{
 
-		@Test (priority = 1)
-		void getUser() {
+	int id ;
 
-			when()
+	@Test (priority = 1)
+	void getUser() {
+
+		when()
 				.get("https://reqres.in/api/users/2")
 
-			.then().statusCode(200)
+				.then().statusCode(200)
 				.log().all();
 
-		}
+	}
 
 		/*
 		
 		@Test(priority = 2)
 		void listUser() {
-		
+
 		when()
 			.get("https://reqres.in/api/users?page=2")
 
@@ -36,48 +38,50 @@ import java.util.HashMap;
 			.log().all();
 
 		}*/
-		
-		
-		
-		@Test(priority = 3)
-		void createUser() {
-			
-			HashMap<String, String> data = new HashMap<String, String>();
-			
-			data.put("name", "Pavan");
-			data.put("Job", "Trainer");
-			
-			id = given()
-				.contentType("application/json")
-				.body(data)
-				
-			.when()
-				.post("https://reqres.in/api/users")
-				.jsonPath().getInt("id");
-				
-			//.then()
-				//.log().all();
-		}
-			
-		@Test(priority = 4, dependsOnMethods = "createUser")
-		void updateUser() {
-			
-			HashMap<String, String> data = new HashMap<String, String>();
-			
-			data.put("name", "Pavan");
-			data.put("Job", "Teacher");
-			
-			given()
-				.contentType("application/json")
-				.body(data)
-				
-			.when()
-				.put("https://reqres.in/api/users/"+id)
-				
-			.then()
-				.log().all();
+
+
+
+	@Test(priority = 3)
+	void createUser() {
+
+		HashMap<String, String> data = new HashMap<String, String>();
+
+		data.put("name", "Pavan");
+		data.put("Job", "Trainer");
+
+		for(Map.Entry<String,String> entry : data.entrySet()){
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue());
 		}
 
+		id = given()
+				.contentType("application/json")
+				.body(data)
+
+				.when()
+				.post("https://reqres.in/api/users")
+				.jsonPath().getInt("id");
 	}
+
+	@Test(priority = 4, dependsOnMethods = "createUser")
+	void updateUser() {
+
+		HashMap<String, String> data = new HashMap<String, String>();
+
+		data.put("name", "Pavan");
+		data.put("Job", "Teacher");
+
+		given()
+				.contentType("application/json")
+				.body(data)
+
+				.when()
+				.put("https://reqres.in/api/users/"+id)
+
+				.then()
+				.log().all();
+	}
+
+}
 
 
